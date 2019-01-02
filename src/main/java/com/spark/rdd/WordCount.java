7089792d06897759e0b0ac9c379d6be042c6a3ce
Namrita Garg <NamritaGarg@fico.com>
2019-01-02 14:13:22 +0530

    Spark program to count distinct words and their counts in a text file

---
+++ src/main/java/com/spark/rdd/WordCount.java
@@ -0,0 +1,28 @@
+package com.spark.rdd;
+
+import java.util.Arrays;
+import java.util.Map;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class WordCount {
+  public static void main(String[] args) throws Exception {
+
+    Logger.getLogger("org").setLevel(Level.ERROR);
+    SparkConf conf = new SparkConf().setAppName("wordCounts").setMaster("local[2]");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+
+    JavaRDD<String> lines = sc.textFile("src/main/resources/word_count.txt");
+    final JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
+    final Map<String, Long> wordCounts = words.countByValue();
+
+    for (Map.Entry<String, Long> entry : wordCounts.entrySet()) {
+      System.out.println(entry.getKey() + " : " + entry.getValue());
+    }
+
+  }
+}
